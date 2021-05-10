3a0fd6923c78c02d0be3f37378256573465b90ba
imfricky <frickyrathore02@gmail.com>
2021-05-10 17:59:57 +0530

    Update CalenderAsstApplication.java

--- src/main/java/com/example/demo/CalenderAsstApplication.java
+++ src/main/java/com/example/demo/CalenderAsstApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
