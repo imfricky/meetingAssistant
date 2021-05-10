a57b9237b81846c635bce7c9da9618703d78db49
imfricky <frickyrathore02@gmail.com>
2021-05-10 17:39:01 +0530

    Update CalenderAsstApplication.java

--- src/main/java/com/example/demo/CalenderAsstApplication.java
+++ src/main/java/com/example/demo/CalenderAsstApplication.java
@@ -7,8 +7,11 @@ import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
-public class CalenderAsstApplication {
-
+public class CalenderAsstApplication extends SpringBootServletInitializer {
+	@Override
+    	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        	return application.sources(HelloWorldApplication.class);
+    }
 	@PostConstruct
 	public void init(){
 		// Setting Spring Boot SetTimeZone
@@ -18,5 +21,8 @@ public class CalenderAsstApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CalenderAsstApplication.class, args);
 	}
-
+	@RequestMapping("/")
+    	String helloWorld() {
+        	return "Hello World!";
+    }
 }
