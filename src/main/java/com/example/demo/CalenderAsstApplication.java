b983ed64465ef2947a1b5c501f0ed6a9d2b71e66
Rahul singh <frickyrathore02@gmail.com>
2021-05-24 11:27:49 +0530

    Adding Tests

--- src/main/java/com/example/demo/CalenderAsstApplication.java
+++ src/main/java/com/example/demo/CalenderAsstApplication.java
@@ -2,13 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
+
 @SpringBootApplication
 public class CalenderAsstApplication extends SpringBootServletInitializer {
-	
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CalenderAsstApplication.class);
+	}
 	@PostConstruct
 	public void init(){
 		// Setting Spring Boot SetTimeZone
@@ -18,5 +28,6 @@ public class CalenderAsstApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(CalenderAsstApplication.class, args);
 	}
-	
+
+
 }
